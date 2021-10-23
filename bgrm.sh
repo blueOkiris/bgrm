a44d079d6a5e853c9de60d57a9d73f7221639278
Dylan Turner <dylantdmt@gmail.com>
2021-10-23 12:56:29 -0500

    Switched scripts to using modprobe instead of insmod

--- bgrm.sh
+++ bgrm.sh
@@ -8,11 +8,6 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
-if [ ! -f "v4l2loopback.ko" ]; then
-    echo "You need to build the loopback kernel module first! (see README.md)"
-    exit 1
-fi
-
 if [ ! -d ".venv" ]; then
     echo "Initializing virtual environment!"
     source scripts/setup-venv.sh
@@ -21,7 +16,7 @@ if [ ! -d ".venv" ]; then
     cp scripts/v4l2\ \(patch\).py .venv/lib/python3.9/site-packages/v4l2.py
 fi
 
-insmod v4l2loopback.ko \
+modprobe v4l2loopback \
     devices=1 exclusive_caps=1 video_nr=10 max_buffers=2 \
     card_label=v4l2lo
 
@@ -31,5 +26,3 @@ python3.9 bgrm \
     ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} \
     ${23} ${24} ${25} ${26}
 deactivate
-
-rmmod v4l2loopback
