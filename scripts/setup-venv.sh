6ead50b890c50c259f66f112db0abbc8d1b0bbb8
Dylan Turner <dylantdmt@gmail.com>
2021-12-31 11:36:49 -0600

    Undid last chagne to install

--- scripts/setup-venv.sh
+++ scripts/setup-venv.sh
@@ -3,11 +3,11 @@
 # Author: Dylan Turner
 # Description: Creates and installs all needed packages
 
-python -m pip install virtualenv --user
+python -m pip install virtualenv
 
 python -m virtualenv .venv
 chmod +x .venv/bin/activate
 source .venv/bin/activate
-pip3 install pip --upgrade --user
-pip3 install -r bgrm/requirements.txt --no-cache-dir --user
+pip3 install pip --upgrade
+pip3 install -r bgrm/requirements.txt --no-cache-dir
 deactivate
