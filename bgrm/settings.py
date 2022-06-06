935bc8c5e70489fff8f67b577238425b6d4ad439
Dylan Turner <dylantdmt@gmail.com>
2022-06-06 14:56:45 -0500

    Adjusted order of arguments

--- bgrm/settings.py
+++ bgrm/settings.py
@@ -24,21 +24,23 @@ class AppSettings:
     @staticmethod
     def fromArguments():
         parser = ArgumentParser()
+
+        # Set up arguments
         parser.add_argument(
-            '-t', '--title', type = str, default = 'Feeds',
-            help = 'Window title'
+            '-c', '--camera', type = int, default = 0,
+            help = 'ID of camera device to use for input'
         )
         parser.add_argument(
-            '-x', '--start_x', type = int, default = 1000,
-            help = 'Window horizontal start position'
-        )
-        parser.add_argument(
-            '-y', '--start_y', type = int, default = 300,
-            help = 'Window vertical start position'
+            '-b', '--bg', type = str, default = '',
+            help = 'Background image'
         )
         parser.add_argument(
             '-c', '--camera', type = int, default = 0,
             help = 'ID of camera device to use for input'
+        )
+        parser.add_argument(
+            '-T', '--thresh', type = float, default = 0.4,
+            help = 'Background removal threshold'
         )
         parser.add_argument(
             '-w', '--width', type = int, default = 1280,
@@ -48,18 +50,8 @@ class AppSettings:
             '-H', '--height', type = int, default = 960,
             help = 'Height of camera input'
         )
-        parser.add_argument(
-            '-s', '--scale', type = float, default = 0.5,
-            help = 'Scale of window to display'
-        )
-        parser.add_argument(
-            '-T', '--thresh', type = float, default = 0.4,
-            help = 'Background removal threshold'
-        )
-        parser.add_argument(
-            '-b', '--bg', type = str, default = '',
-            help = 'Background image'
-        )
+
+        # Extra options
         parser.add_argument(
             '--blur', help = 'Blur background (overrides --bg)',
             action='store_true'
@@ -68,6 +60,25 @@ class AppSettings:
             '--disable_window', help = 'Disable feedback window',
             action='store_true'
         )
+
+        # Mostly useless window options
+        parser.add_argument(
+            '-t', '--title', type = str, default = 'Feeds',
+            help = 'Window title'
+        )
+        parser.add_argument(
+            '-x', '--start_x', type = int, default = 0,
+            help = 'Window horizontal start position'
+        )
+        parser.add_argument(
+            '-y', '--start_y', type = int, default = 0,
+            help = 'Window vertical start position'
+        )
+        parser.add_argument(
+            '-s', '--scale', type = float, default = 0.5,
+            help = 'Scale factor from camera size to window.'
+        )
+ 
         args = parser.parse_args()
 
         return AppSettings(
