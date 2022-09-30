fb30eef842b5a71218ffc6a0c929797d3b57000e
Dylan Turner <dylantdmt@gmail.com>
2022-09-30 12:35:21 -0500

    Changed path of bgrm/ folder to match flathub install location

--- bgrm-fp.sh
+++ bgrm-fp.sh
@@ -5,7 +5,7 @@
 
 export DEVICE_ARR=(`ls /sys/devices/virtual/video4linux | tr -d 'video'`)
 echo "Using loopback device /dev/video${DEVICE_ARR[0]}"
-python /opt/bgrm \
+python /app/opt/bgrm \
     ${DEVICE_ARR[0]} $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} \
     ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20} ${21} ${22} \
     ${23} ${24} ${25} ${26}
